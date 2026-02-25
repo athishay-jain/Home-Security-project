35d3e20bffee8a44a407a929179f89dfaf161483
Athishay jain <athishayjainvenoor@gmail.com>
2026-02-25 06:12:11 +0530

    changed to active low buzzer

--- home_security.ino
+++ home_security.ino
@@ -1,7 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <Servo.h>
+#include <ESP8266Servo.h>   // ✅ Correct Servo Library for ESP8266
 
+// ---------------- PIN DEFINITIONS ----------------
 #define TRIG1 D5
 #define ECHO1 D6
 #define TRIG2 D7
@@ -12,57 +13,52 @@
 #define BUZZER_PIN D1
 #define BUTTON_PIN D2
 
+// ---------------- OBJECTS ----------------
 Servo doorServo;
 ESP8266WebServer server(80);
 
+// ---------------- VARIABLES ----------------
 bool homeLocked = true;
 bool panicMode = false;
+
+bool doorAlertActive = false;
+bool windowAlertActive = false;
 
 unsigned long alertStartTime = 0;
 unsigned long panicStartTime = 0;
 unsigned long windowAlertStart = 0;
 
-bool doorAlertActive = false;
-bool windowAlertActive = false;
+const long doorThreshold = 20;
+const long windowThreshold = 5;
 
-const long doorThreshold = 20;     // cm
-const long windowThreshold = 5;    // cm
-
-// -------- Distance Function ----------
+// ---------------- DISTANCE FUNCTION ----------------
 long getDistance(int trigPin, int echoPin) {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
+
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  long duration = pulseIn(echoPin, HIGH);
+  long duration = pulseIn(echoPin, HIGH, 30000);
   long distance = duration * 0.034 / 2;
+
   return distance;
 }
 
-// -------- Door Open ----------
+// ---------------- DOOR CONTROL ----------------
 void openDoor() {
   doorServo.write(90);
   homeLocked = false;
   server.send(200, "text/plain", "Door Opened");
 }
 
-// -------- Door Close ----------
 void closeDoor() {
   doorServo.write(0);
   homeLocked = true;
   server.send(200, "text/plain", "Door Closed");
 }
 
-// -------- Panic ----------
-void triggerPanic() {
-  panicMode = true;
-  panicStartTime = millis();
-  server.send(200, "text/plain", "Panic Activated");
-}
-
-// -------- Lock / Unlock ----------
 void lockHome() {
   homeLocked = true;
   server.send(200, "text/plain", "Home Locked");
@@ -73,6 +69,13 @@ void unlockHome() {
   server.send(200, "text/plain", "Home Unlocked");
 }
 
+void triggerPanic() {
+  panicMode = true;
+  panicStartTime = millis();
+  server.send(200, "text/plain", "Panic Activated");
+}
+
+// ---------------- SETUP ----------------
 void setup() {
 
   pinMode(TRIG1, OUTPUT);
@@ -84,12 +87,15 @@ void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
 
+  digitalWrite(LED_PIN, LOW);
+  digitalWrite(BUZZER_PIN, HIGH);  // OFF (Active LOW buzzer)
+
   doorServo.attach(SERVO_PIN);
   doorServo.write(0);
 
   Serial.begin(115200);
 
-  // -------- WiFi AP Mode ----------
+  // -------- WiFi AP Mode --------
   WiFi.mode(WIFI_AP);
   WiFi.softAP("HomeSecurity", "12345678");
 
@@ -101,7 +107,7 @@ void setup() {
   Serial.println("Access Point Started");
   Serial.println(WiFi.softAPIP());
 
-  // -------- Server Routes ----------
+  // -------- Server Routes --------
   server.on("/open", openDoor);
   server.on("/close", closeDoor);
   server.on("/panic", triggerPanic);
@@ -111,66 +117,66 @@ void setup() {
   server.begin();
 }
 
+// ---------------- LOOP ----------------
 void loop() {
 
   server.handleClient();
 
   long doorDistance = getDistance(TRIG1, ECHO1);
   long windowDistance = getDistance(TRIG2, ECHO2);
-// -------- Door Detection ----------
-static bool personDetected = false;
 
-if (doorDistance < doorThreshold) {
+  static bool personDetected = false;
 
-  if (!personDetected) {
-    personDetected = true;
+  // -------- DOOR SENSOR --------
+  if (doorDistance > 0 && doorDistance < doorThreshold) {
 
-    if (homeLocked) {
-      // Locked → 7 second alarm
-      doorAlertActive = true;
-      alertStartTime = millis();
-      digitalWrite(LED_PIN, HIGH);
-      digitalWrite(BUZZER_PIN, HIGH);
-    } 
-    else {
-      // Door open → single short beep
-      digitalWrite(BUZZER_PIN, HIGH);
-      delay(200);
-      digitalWrite(BUZZER_PIN, LOW);
+    if (!personDetected) {
+      personDetected = true;
+
+      if (homeLocked) {
+        doorAlertActive = true;
+        alertStartTime = millis();
+        digitalWrite(LED_PIN, HIGH);
+        digitalWrite(BUZZER_PIN, LOW);   // ON
+      } 
+      else {
+        digitalWrite(BUZZER_PIN, LOW);
+        delay(200);
+        digitalWrite(BUZZER_PIN, HIGH);
+      }
     }
+
+  } else {
+    personDetected = false;
   }
 
-} else {
-  personDetected = false;
-}
+  if (doorAlertActive && millis() - alertStartTime > 7000) {
+    digitalWrite(LED_PIN, LOW);
+    digitalWrite(BUZZER_PIN, HIGH);
+    doorAlertActive = false;
+  }
 
-// Stop 7 sec locked alarm
-if (doorAlertActive && millis() - alertStartTime > 7000) {
-  digitalWrite(LED_PIN, LOW);
-  digitalWrite(BUZZER_PIN, LOW);
-  doorAlertActive = false;
-}
-
-  // -------- Window Detection ----------
-  if (windowDistance < windowThreshold && !windowAlertActive) {
+  // -------- WINDOW SENSOR --------
+  if (windowDistance > 0 && windowDistance < windowThreshold && !windowAlertActive) {
     windowAlertActive = true;
     windowAlertStart = millis();
   }
 
   if (windowAlertActive) {
+
     if ((millis() - windowAlertStart) % 1000 < 200) {
-      digitalWrite(BUZZER_PIN, HIGH);
-    } else {
       digitalWrite(BUZZER_PIN, LOW);
+    } else {
+      digitalWrite(BUZZER_PIN, HIGH);
     }
 
     if (millis() - windowAlertStart > 7000) {
-      digitalWrite(BUZZER_PIN, LOW);
+      digitalWrite(BUZZER_PIN, HIGH);
       windowAlertActive = false;
     }
   }
 
-  // -------- Panic Button ----------
+  // -------- PANIC BUTTON --------
   if (digitalRead(BUTTON_PIN) == LOW && !panicMode) {
     panicMode = true;
     panicStartTime = millis();
@@ -178,11 +184,11 @@ if (doorAlertActive && millis() - alertStartTime > 7000) {
 
   if (panicMode) {
     digitalWrite(LED_PIN, HIGH);
-    digitalWrite(BUZZER_PIN, HIGH);
+    digitalWrite(BUZZER_PIN, LOW);
 
     if (millis() - panicStartTime > 10000) {
       digitalWrite(LED_PIN, LOW);
-      digitalWrite(BUZZER_PIN, LOW);
+      digitalWrite(BUZZER_PIN, HIGH);
       panicMode = false;
     }
   }
